28695de2f2f26fbbd921da9fc42d79844b72209a
migfalher <migfalher@alu.edu.gva.es>
2025-11-06 19:07:05 +0100

    Borrar scripts innecesarios
    Solo quedan "GameManager" y "Canyon_Scripts". Seguramente necesitaré más, pero eso lo decidiré en el futuro...

--- Assets/Scripts/Canyon.cs
+++ Assets/Scripts/Canyon.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Canyon_Script : MonoBehaviour
+public class Canyon : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,78 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Shared objects
+    public GameObject Canyon;
+
+    // Attributes
+    private int ballCounter;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ballCounter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            DetectClick();
+        }
+    }
+
+    // Detect mouse click
+    private void DetectClick()
+    {
+        // Get click variables
+        // -> 'hit' is a sentinel to detect if the ray has touched a collider
+        // -> 'ray' is thrown from the camera core to the place where the mouse "would be" in the world
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // has the ray even touched any collider?
+        if (Physics.Raycast(ray, out hit))
+        {
+            // if so:
+            // activate event of clicked object
+            GameObject coll = hit.transform.gameObject;
+            string parentName = coll.transform.parent.name;
+            switch (parentName)
+            {
+                case "Boton_Green":
+                    OnGreenClicked();
+                    break;
+                case "Boton_Yellow":
+                    OnYellowClicked();
+                    break;
+                case "Boton_Red":
+                    OnRedClicked();
+                    break;
+                default:
+                    Debug.Log("No event implemented for clicked object");
+                    break;
+            }
+        }
+    }
+
+    // Event on green button clicked
+    private void OnGreenClicked()
+    {
+        ballCounter++;
+        Debug.Log(ballCounter);
+    }
+
+    // Event on yellow button clicked
+    private void OnYellowClicked()
+    {
+        Debug.Log("Yellow");
+    }
+
+    // Event on red clicked
+    private void OnRedClicked()
+    {
+        Debug.Log("Red");
+        ballCounter = 0;
     }
 }
